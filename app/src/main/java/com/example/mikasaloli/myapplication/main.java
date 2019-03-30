c4c0c9b7beeda224cc65f0c18255ae36bdc39e4f
mikasaloliz <11161502106@hu.edu.vn>
2019-03-30 16:58:01 +0700

    update

--- app/src/main/java/com/example/mikasaloli/myapplication/main.java
+++ app/src/main/java/com/example/mikasaloli/myapplication/main.java
@@ -64,128 +64,20 @@ public class main extends Fragment {
         result= (TextView) v.findViewById(R.id.textView3);
         thang=(TextView) v.findViewById(R.id.textView5);
 
-        thang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar c = Calendar.getInstance();
-                int year = c.get(Calendar.YEAR)+4;
-                int month = c.get(Calendar.MONTH)-2;
-                int day = c.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog dialog = new DatePickerDialog(
-                        getActivity(),
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mdatalis,
-                        year,month,day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
 
-
-
-            }
-        });
-        mdatalis = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month1, int dayOfMonth) {
-                month1 = month1 + 1;
-                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month1 + "/" + dayOfMonth + "/" + year);
-
-                final String date = String.valueOf(month1);
-                thang.setText("Tháng : "+date);
-
-            }
-        };
 
         if (user != null) {
 
-
-            save.setOnClickListener(new View.OnClickListener() {
+            thang.setText(curUid);
+            ref.addValueEventListener(new ValueEventListener() {
                 @Override
-                public void onClick(View v) {
-
-
-
-                    int ele1 = Integer.parseInt(eleinput2.getText().toString());
-                    int water1 = Integer.parseInt(waterinput2.getText().toString());
-                    String m = month.getText().toString();
-                    mRef.child("water").setValue(water1);
-
-                    mRef.child("ele").setValue(ele1);
-
-                    mRef.child(m).child("ele").setValue(ele1);
-
-                    mRef.child(m).child("water").setValue(water1);
-                    Toast.makeText(getActivity(),"Saved",Toast.LENGTH_LONG).show();
-
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    int f1= dataSnapshot.child("order").getValue(Integer.class);
+                    result.setText(f1);
                 }
-            });
-            add.setOnClickListener(new View.OnClickListener() {
+
                 @Override
-                public void onClick(View v) {
-
-
-                    ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                            int ele = dataSnapshot.child("ele").getValue(Integer.class);
-
-                            int water = dataSnapshot.child("water").getValue(Integer.class); // now do what you want from these
-
-                            int thisele=Integer.parseInt(eleinput2.getText().toString());
-
-                            int thiswater= Integer.parseInt(waterinput2.getText().toString());
-
-                            int finalele=thisele - ele;
-
-                            int finalwater=thiswater - water;
-
-                            int sumele = finalele * 1820;
-                            int sumwater = finalwater * 10000;
-                            final int sum = sumele + sumwater;
-
-                            DecimalFormat formatter = new DecimalFormat("###,###,###");
-                            formatter.format(sum);
-
-                            result.setText(String.valueOf(formatter.format(sum)+" VND"));
-                            save.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-
-
-
-                                    int ele1 = Integer.parseInt(eleinput2.getText().toString());
-                                    int water1 = Integer.parseInt(waterinput2.getText().toString());
-                                    int m = Integer.parseInt(month.getText().toString());
-                                    int m1= m-1;
-                                    String m2 =String.valueOf(m1);
-
-                                    String m3 =String.valueOf(m);
-
-                                    mRef.child("water").setValue(water1);
-
-                                    mRef.child("ele").setValue(ele1);
-
-                                    mRef.child(m3).child("ele").setValue(ele1);
-
-                                    mRef.child(m3).child("water").setValue(water1);
-
-                                    mRef.child(m2).child("money").setValue(sum);
-                                    Toast.makeText(getActivity(),"Saved",Toast.LENGTH_LONG).show();
-
-                                }
-                            });
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-
-                    });
-
-
-
+                public void onCancelled(DatabaseError error) {
 
                 }
             });
